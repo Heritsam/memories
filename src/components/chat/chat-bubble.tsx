c746d4cc0aa939a19dfef893701289e568ddb8c7
Ariq Heritsa <ariqhm@gmail.com>
2023-08-22 02:11:11 +0700

    fix(chat): fix translations

--- src/components/chat/chat-bubble.tsx
+++ src/components/chat/chat-bubble.tsx
@@ -17,7 +17,7 @@ const ChatBubble = (props: Props) => {
   return (
     <div className='flex flex-row justify-start w-full'>
       <div className='w-max-1/2 mr-24 px-6 py-4 rounded-tl-2xl rounded-tr-2xl rounded-bl-sm rounded-br-2xl text-left bg-slate-600'>
-        Halo! Selamat datang di Museum Konferensi Asia Afrika, saya Mories, pemandu wisata virtual Anda. Jika Anda tertarik untuk mengetahui lebih banyak tentang Konferensi Asia Afrika, silakan tanyakan lebih lanjut! Saya di sini untuk membantu Anda menjawab pertanyaan dan memberikan informasi yang relevan.
+        {props.message}
       </div>
     </div>
   );
