77a5d6322b9e863c48a167fb185d90fee371e483
Ariq Heritsa <ariqhm@gmail.com>
2023-09-15 15:32:49 +0700

    style: format using prettier

--- src/components/about-dialog.tsx
+++ src/components/about-dialog.tsx
@@ -21,82 +21,86 @@ const AboutDialog = ({ children }: Props) => {
 
   return (
     <Dialog>
-      <DialogTrigger className='w-20 cursor-pointer rounded-xl bg-gradient-to-br from-slate-700 to-slate-800 shadow-xl hover:shadow-lg'>
+      <DialogTrigger className="w-20 cursor-pointer rounded-xl bg-gradient-to-br from-slate-700 to-slate-800 shadow-xl hover:shadow-lg">
         {children}
       </DialogTrigger>
 
-      <DialogContent className='bg-slate-700'>
-        <div className='mb-10 mt-4 flex flex-col items-center justify-center'>
-          <img src={memories} alt='memories' className='mx-auto h-20' />
+      <DialogContent className="bg-slate-700">
+        <div className="mb-10 mt-4 flex flex-col items-center justify-center">
+          <img src={memories} alt="memories" className="mx-auto h-20" />
 
-          <h4 className='text-center text-lg font-bold uppercase text-gray-300'>
+          <h4 className="text-center text-lg font-bold uppercase text-gray-300">
             {t('about')}
           </h4>
 
-          <p className='mb-4 w-5/6 text-center text-xs font-medium text-gray-400'>
-            Memories (Monitor Education Using Monitor Intelligence System) merupakan inovasi teknologi berupa <b className='font-bold text-gray-100'>monitor pintar</b> yang berperan sebagai pemandu museum untuk memperoleh informasi yang relevan.
+          <p className="mb-4 w-5/6 text-center text-xs font-medium text-gray-400">
+            Monitor Education Using Monitor Intelligence System merupakan
+            inovasi teknologi berupa{' '}
+            <b className="font-bold text-gray-100">monitor pintar</b> yang
+            berperan sebagai pemandu museum untuk memperoleh informasi yang
+            relevan.
           </p>
 
-          <h4 className='mb-3 text-center text-lg font-bold uppercase text-gray-300'>
+          <h4 className="mb-3 text-center text-lg font-bold uppercase text-gray-300">
             {t('team_members')}
           </h4>
 
-          <div className='mb-4 flex flex-wrap justify-center gap-4'>
-            <div className='w-[100%]'>
+          <div className="mb-4 flex flex-wrap justify-center gap-4">
+            <div className="w-[100%]">
               <AvatarTeam
                 imageUrl={agunghadi}
-                alias='AHW'
-                fullName='Agung Hadi Winoto'
-                major='S1 Data Sains'
+                alias="AHW"
+                fullName="Agung Hadi Winoto"
+                major="S1 Data Sains"
                 ketua={true}
               />
             </div>
 
-            <div className='w-[48%]'>
+            <div className="w-[48%]">
               <AvatarTeam
                 imageUrl={krisnaaprileo}
-                alias='KAI'
-                fullName='Krisna Aprileo'
-                major='S1 Akuntansi'
+                alias="KAI"
+                fullName="Krisna Aprileo"
+                major="S1 Akuntansi"
               />
             </div>
 
-            <div className='w-[48%]'>
+            <div className="w-[48%]">
               <AvatarTeam
                 imageUrl={elsivinalsy}
-                alias='EVC'
-                fullName='Elsi Vinalsy'
-                major='S1 Akuntansi'
+                alias="EVC"
+                fullName="Elsi Vinalsy"
+                major="S1 Akuntansi"
               />
             </div>
 
-            <div className='w-[48%]'>
+            <div className="w-[48%]">
               <AvatarTeam
                 imageUrl={ariqheritsa}
-                alias='AHM'
-                fullName='Ariq Heritsa'
-                major='S1 Data Sains'
+                alias="AHM"
+                fullName="Ariq Heritsa"
+                major="S1 Data Sains"
               />
             </div>
 
-            <div className='w-[48%]'>
+            <div className="w-[48%]">
               <AvatarTeam
                 imageUrl={akbarprakoso}
-                alias='AKB'
-                fullName='Akbar Prakoso'
-                major='S1 Informatika'
+                alias="AKB"
+                fullName="Akbar Prakoso"
+                major="S1 Informatika"
               />
             </div>
           </div>
 
-          <h4 className='mb-1 text-center text-lg font-bold uppercase text-gray-300'>
+          <h4 className="mb-1 text-center text-lg font-bold uppercase text-gray-300">
             {t('follow_us')}
           </h4>
 
-          <div className='flex'>
-            <a href='https://instagram.com/memories.pkmkc' target='_blank'>
-              <Button variant='link'>
-                <img src={instagram} className='mr-2 w-6' />
+          <div className="flex">
+            <a href="https://instagram.com/memories.pkmkc" target="_blank">
+              <Button variant="link">
+                <img src={instagram} className="mr-2 w-6" />
                 @memories.pkmkc
               </Button>
             </a>
