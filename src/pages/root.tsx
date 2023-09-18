b39399a8446a3c4d75fe498287734e07bd6622d9
Ariq Heritsa <ariqhm@gmail.com>
2023-09-18 23:05:23 +0700

    feat: add idle detector

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { useIdleTimer } from 'react-idle-timer';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '@/components/navbar';
@@ -5,17 +7,51 @@ import Navbar from '@/components/navbar';
 import landing from '../assets/landing.png';
 
 const Root = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [remaining, setRemaining] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+
+  const onIdle = () => {
+    console.log('User is idle');
+  };
+
+  const onActive = () => {
+    console.log('User is active');
+  };
+
+  const onAction = () => {
+    setCount(count + 1);
+  };
+
+  const { getRemainingTime } = useIdleTimer({
+    onIdle,
+    onActive,
+    onAction,
+    timeout: 180_000, // 3 minutes
+    throttle: 500,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemaining(Math.ceil(getRemainingTime() / 1000));
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
     <main
       style={{ backgroundImage: `url(${landing})`, backgroundSize: 'cover' }}
-      className='h-screen text-white'
+      className="h-screen text-white"
     >
-      <div className='container mx-auto flex h-full flex-col py-4'>
+      <div className="container mx-auto flex h-full flex-col py-4">
         <Navbar />
         <Outlet />
       </div>
     </main>
   );
-}
+};
 
 export default Root;
