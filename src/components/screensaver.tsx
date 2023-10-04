a04ed6260e131c38cea0759a4fe15b93a264d5ff
Ariq Heritsa <ariqhm@gmail.com>
2023-10-04 09:45:37 +0700

    feat: update screensaver video

--- src/components/screensaver.tsx
+++ src/components/screensaver.tsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 
 import landing from '../assets/landing.png';
-import tutorial from '../assets/videos/tutorial.mp4';
 
 const Screensaver = () => {
   const width = useRef(window.innerWidth);
@@ -14,13 +13,13 @@ const Screensaver = () => {
     >
       <div className="flex h-full items-center justify-center">
         <video
-          width={width.current - 128}
-          height={height.current - 128}
+          width={width.current}
+          height={height.current}
           autoPlay={true}
           loop={true}
           className="m-8 rounded-xl shadow-2xl"
         >
-          <source src={tutorial} />
+          <source src="https://rr4---sn-npoe7nsy.c.drive.google.com/videoplayback?expire=1696398067&ei=w9AcZaSBN9aOzN0P-LWz-A0&ip=203.190.54.126&id=1ee9e97f00ca073e&itag=37&source=webdrive&requiressl=yes&xpc=EghotM6WJ3oBAQ==&mh=my&mm=32&mn=sn-npoe7nsy&ms=su&mv=m&mvi=4&pl=24&ttl=transient&susc=dr&driveid=17p2btUupx-K1mIkE0hz89yLr3N5JFSLS&app=explorer&eaua=B6nAP4UNIos&mime=video/mp4&vprv=1&prv=1&dur=45.116&lmt=1696371769353303&mt=1696387025&subapp=DRIVE_WEB_FILE_VIEWER&txp=0001224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRAIgI3cI5L5WCaWdxXpCJlF92hDeLus7RyBAKDnkF-ErFucCIH_XhzpksLF-Q3KbTQa31Er2H1_rayWl6gN7Xg8XVk0k&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgPEaDci9NXVheWiUG7PULipLtALJ6mVw_EFiYjZTLSJcCIHgPN6GkVKnSnvp9L4cJIUcefnppBcWbneA3BUy3crrb&cpn=wcZ8RamzxQOEK_DI&c=WEB_EMBEDDED_PLAYER&cver=1.20230926.01.00" />
         </video>
       </div>
     </div>
